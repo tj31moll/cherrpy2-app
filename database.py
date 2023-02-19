f4bee04685d9a8ac01ea83a34d69511cf5f6a6d6
tj31moll <tj31moll@gmail.com>
2023-02-19 09:09:46 -0500

    jtlwkejrtlkwjrlk

--- database.py
+++ database.py
@@ -1,94 +1,16 @@
-import sqlite3
+from models import db, Kid
 
-class AllowanceTrackerDB:
-    def __init__(self):
-        self.conn = sqlite3.connect('allowancetracker.db')
-        self.conn.execute('PRAGMA foreign_keys = ON')  # enable foreign key constraints
-        self.create_tables()
+def seed_data():
+    db.connect()
+    db.create_tables([Kid])
 
-    def create_tables(self):
-        self.conn.execute('''
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL,
-                balance REAL NOT NULL
-            )
-        ''')
+    kids = [
+        {'name': 'Alice', 'balance': 50},
+        {'name': 'Bob', 'balance': 100},
+        {'name': 'Charlie', 'balance': 75},
+    ]
 
-        self.conn.execute('''
-            CREATE TABLE IF NOT EXISTS activities (
-                id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL,
-                cost REAL NOT NULL
-            )
-        ''')
+    for kid in kids:
+        Kid.create(**kid)
 
-        self.conn.execute('''
-            CREATE TABLE IF NOT EXISTS transactions (
-                id INTEGER PRIMARY KEY,
-                user_id INTEGER NOT NULL,
-                activity_id INTEGER NOT NULL,
-                amount REAL NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users (id),
-                FOREIGN KEY (activity_id) REFERENCES activities (id)
-            )
-        ''')
-
-        self.conn.commit()
-
-    def get_all_users(self):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM users')
-        return cursor.fetchall()
-
-    def add_user(self, name, balance):
-        cursor = self.conn.cursor()
-        cursor.execute('INSERT INTO users (name, balance) VALUES (?, ?)', (name, balance))
-        self.conn.commit()
-        return cursor.lastrowid
-
-    def get_user(self, user_id):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
-        return cursor.fetchone()
-
-    def update_user_balance(self, user_id, amount):
-        cursor = self.conn.cursor()
-        cursor.execute('UPDATE users SET balance = balance + ? WHERE id = ?', (amount, user_id))
-        self.conn.commit()
-
-    def get_all_activities(self):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM activities')
-        return cursor.fetchall()
-
-    def add_activity(self, name, cost):
-        cursor = self.conn.cursor()
-        cursor.execute('INSERT INTO activities (name, cost) VALUES (?, ?)', (name, cost))
-        self.conn.commit()
-        return cursor.lastrowid
-
-    def get_activity(self, activity_id):
-        cursor = self.conn.cursor()
-        cursor.execute('SELECT * FROM activities WHERE id = ?', (activity_id,))
-        return cursor.fetchone()
-
-    def add_transaction(self, user_id, activity_id, amount):
-        cursor = self.conn.cursor()
-        cursor.execute('INSERT INTO transactions (user_id, activity_id, amount) VALUES (?, ?, ?)', (user_id, activity_id, amount))
-        self.update_user_balance(user_id, -amount)  # deduct amount from user's balance
-        self.conn.commit()
-        return cursor.lastrowid
-
-    def get_transactions_by_user(self, user_id):
-        cursor = self.conn.cursor()
-        cursor.execute('''
-            SELECT transactions.id, activities.name, transactions.amount
-            FROM transactions
-            JOIN activities ON transactions.activity_id = activities.id
-            WHERE transactions.user_id = ?
-        ''', (user_id,))
-        return cursor.fetchall()
-
-    def close(self):
-        self.conn.close()
+    db.close()
--- models.py
+++ models.py
@@ -1,43 +1,20 @@
-import datetime
-import os
+from peewee import *
 
-import peewee
+db = SqliteDatabase('my_database.db')
 
-db = peewee.SqliteDatabase(os.path.join(os.path.dirname(__file__), 'database.db'))
+class Kid(Model):
+    name = CharField(unique=True)
+    balance = FloatField()
 
-
-class BaseModel(peewee.Model):
     class Meta:
         database = db
 
+class Transaction(Model):
+    kid = ForeignKeyField(Kid, backref='transactions')
+    amount = FloatField()
+    description = CharField()
 
-class Kid(BaseModel):
-    name = peewee.CharField(unique=True)
-    balance = peewee.DecimalField(default=0.00, decimal_places=2)
-    created_at = peewee.DateTimeField(default=datetime.datetime.now)
+    class Meta:
+        database = db
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'balance': float(self.balance),
-            'created_at': self.created_at.isoformat(),
-        }
-    
-    def get_all(cls):
-        kids = Kid.select()
-        return [kid.to_dict() for kid in kids]
-
-
-class Activity(BaseModel):
-    kid = peewee.ForeignKeyField(Kid, backref='activities')
-    name = peewee.CharField()
-    cost = peewee.DecimalField(decimal_places=2)
-
-    def to_dict(self):
-        return {
-            'id2': self.id2,
-            'name': self.name,
-            'cost': float(self.cost),
-            'kid': self.kid.to_dict(),
-        }
+db.create_tables([Kid, Transaction])
